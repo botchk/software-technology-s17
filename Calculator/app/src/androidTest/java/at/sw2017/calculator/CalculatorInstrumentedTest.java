4f0de2d22799966900d0c16fcf449055797f8a39
yeahservice <dom.augustin@gmx.at>
2017-03-23 15:30:26 +0100

    [DA,JS] Added gui testcase for buttons and linked button objects with clicklisteners

--- Calculator/app/src/androidTest/java/at/sw2017/calculator/CalculatorInstrumentedTest.java
+++ Calculator/app/src/androidTest/java/at/sw2017/calculator/CalculatorInstrumentedTest.java
@@ -1,12 +1,18 @@
 package at.sw2017.calculator;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +22,31 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class CalculatorInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<Calculator> mActivityRule = new ActivityTestRule<Calculator>(Calculator.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("at.sw2017.calculator", appContext.getPackageName());
+    }
+
+    @Test
+    public void testButtons() throws  Exception {
+
+        for (int i = 0; i <= 9; ++i) {
+            onView(withText(Integer.toString(i))).perform(click());
+        }
+
+        onView(withText("+")).perform(click());
+        onView(withText("-")).perform(click());
+        onView(withText("*")).perform(click());
+        onView(withText("/")).perform(click());
+
+        onView(withText("=")).perform(click());
+        onView(withText("C")).perform(click());
     }
 }
--- Calculator/app/src/main/java/at/sw2017/calculator/Calculator.java
+++ Calculator/app/src/main/java/at/sw2017/calculator/Calculator.java
@@ -2,12 +2,67 @@ package at.sw2017.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Calculator extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Calculator extends AppCompatActivity implements View.OnClickListener{
+
+    private ArrayList<Button> numberButtons;
+
+    private Button buttonAdd;
+    private Button buttonSubtract;
+    private Button buttonMultiply;
+    private Button buttonDivide;
+    private Button buttonClear;
+    private Button buttonResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        setUpNumberButtonListener();
+
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonAdd.setOnClickListener(this);
+
+        buttonSubtract = (Button) findViewById(R.id.buttonSubtract);
+        buttonSubtract.setOnClickListener(this);
+
+        buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
+        buttonMultiply.setOnClickListener(this);
+
+        buttonDivide = (Button) findViewById(R.id.buttonDivide);
+        buttonDivide.setOnClickListener(this);
+
+        buttonClear = (Button) findViewById(R.id.buttonClear);
+        buttonClear.setOnClickListener(this);
+
+        buttonResult = (Button) findViewById(R.id.buttonResult);
+        buttonResult.setOnClickListener(this);
+    }
+
+    /**
+     * Inits the number buttons and adds the click listener to each button.
+     * The initialized buttons are accessible over the numberButtons array.
+     */
+    public void setUpNumberButtonListener() {
+        for (int i = 0; i <= 9; ++i) {
+            String buttonName = "button" + i;
+
+            int id = getResources().getIdentifier(buttonName, "id", R.class.getPackage().getName());
+
+            Button button = (Button) findViewById(id);
+            button.setOnClickListener(this);
+
+            numberButtons.add(button);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
