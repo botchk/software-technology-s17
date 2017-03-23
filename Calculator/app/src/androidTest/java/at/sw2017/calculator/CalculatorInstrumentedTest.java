2371194e219a7425fdfc598191b9c0d10ab64a02
Jorrit Stramer <jstramer@student.tugraz.at>
2017-03-23 16:31:22 +0100

    [JS] Fixed a testcase for the input field

--- Calculator/app/src/androidTest/java/at/sw2017/calculator/CalculatorInstrumentedTest.java
+++ Calculator/app/src/androidTest/java/at/sw2017/calculator/CalculatorInstrumentedTest.java
@@ -12,15 +12,13 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static java.util.regex.Pattern.matches;
 import static org.junit.Assert.*;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
 public class CalculatorInstrumentedTest {
 
@@ -64,7 +62,7 @@ public class CalculatorInstrumentedTest {
     @Test
     public void testClearButton() {
         onView(withText("3")).perform(click());
-        onView(withTest("C")).perform(click());
+        onView(withText("C")).perform(click());
 
         onView(withId(R.id.textView)).check(matches(withText("0")));
     }
