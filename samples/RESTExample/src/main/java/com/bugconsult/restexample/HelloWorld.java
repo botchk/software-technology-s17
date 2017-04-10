ef53e7f2640af71f83c915a6dade2c16400d3d7c
Dominik Augustin <dominik.augustin@student.tugraz.at>
2017-04-10 22:03:19 +0200

    [DA] Example HelloWorld of a RESTFul Web Service with Jersey and Maven

---
+++ samples/RESTExample/src/main/java/com/bugconsult/restexample/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.bugconsult.restexample;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by Dominik on 10.04.2017.
+ */
+@Path("/hello")
+public class HelloWorld {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessage() {
+        return "Hello world!";
+    }
+}
---
+++ samples/RESTExample/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dominik
+  Date: 10.04.2017
+  Time: 20:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
