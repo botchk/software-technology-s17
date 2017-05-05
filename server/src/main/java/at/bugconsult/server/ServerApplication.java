97928e7d3733ab72d20d3352a7ab246bd2cbf8f4
Dominik Augustin <dominik.augustin@student.tugraz.at>
2017-05-05 21:30:13 +0200

    [DA,JS] Initial REST webservice project

---
+++ server/src/main/java/at/bugconsult/server/ServerApplication.java
@@ -0,0 +1,8 @@
+package at.bugconsult.server;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/*")
+public class ServerApplication extends Application {
+}
